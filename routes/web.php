b57b43d5ed5c5550020f5e8bc599fe3dab07e4ca
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-11 16:01:09 +0200

    feat: try add bonus

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage');
 })->name('homepage');
+
+Route::get('/', function () {
+    return view('features');
+})->name('features');
